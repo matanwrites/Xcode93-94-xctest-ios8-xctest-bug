a23ec0875002351df3ff7055e2f1ecdbc5e9442d
Mathieu Tan <mathieu.tan@ef.com>
2018-04-09 19:19:08 +0800

    init

---
+++ ios8-xctest-bug/ios8-xctest-bug/ios8_xctest_bug.h
@@ -0,0 +1,19 @@
+//
+//  ios8_xctest_bug.h
+//  ios8-xctest-bug
+//
+//  Created by Mathieu.Tan on 09/04/2018.
+//  Copyright © 2018 Mathieu.Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios8_xctest_bug.
+FOUNDATION_EXPORT double ios8_xctest_bugVersionNumber;
+
+//! Project version string for ios8_xctest_bug.
+FOUNDATION_EXPORT const unsigned char ios8_xctest_bugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios8_xctest_bug/PublicHeader.h>
+
+
